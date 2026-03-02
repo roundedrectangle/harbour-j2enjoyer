4f45fd9131e6d04c8924904bea5fc745c099244a
legacychimera247 <mad_fer_it@vivaldi.net>
2026-03-02 15:12:07 +0100

    italian translation updated

---
+++ translations/harbour-j2enjoyer-it.ts
@@ -0,0 +1,209 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="it_IT">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="56"/>
+        <source>A helper application for all the J2 Enjoyers.</source>
+        <translation>Un&apos;applicazione per tutti quelli che amano il J2.</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="64"/>
+        <source>This project is licensed under GNU GPL 3.0. Copyright (c) 2025 roundedrectangle</source>
+        <translation>Questo progetto è sotto licenza GNU-GPL 3.0. Copyright (c) 2025 roundedrectangle</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="75"/>
+        <source>Translations</source>
+        <translation>Traduzioni</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="78"/>
+        <source>Italian</source>
+        <translation>Italiano</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="90"/>
+        <source>This project uses a modified version of the dom-parser project, licensed under ISC.</source>
+        <translation>Questo progetto usa una versione modificata del progetto dom parser, sotto licenza ISC.</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="99"/>
+        <source>ISC License</source>
+        <translation>Licenza ISC</translation>
+    </message>
+</context>
+<context>
+    <name>CoverPage</name>
+    <message numerus="yes">
+        <location filename="../qml/cover/CoverPage.qml" line="58"/>
+        <source>sold out of %n</source>
+        <comment>after a number of units, e.g. 1500 [in big] &lt;new line&gt; sold out of 2000</comment>
+        <translation>
+            <numerusform>venduto di %n</numerusform>
+            <numerusform>venduti di %n</numerusform>
+        </translation>
+    </message>
+</context>
+<context>
+    <name>FirstPage</name>
+    <message>
+        <location filename="../qml/pages/FirstPage.qml" line="25"/>
+        <source>About</source>
+        <translation>Info</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/FirstPage.qml" line="29"/>
+        <source>Settings</source>
+        <translation>Impostazioni</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/FirstPage.qml" line="33"/>
+        <source>Enjoy in Browser</source>
+        <translation>Enjoy nel Browser</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/FirstPage.qml" line="38"/>
+        <source>Refresh</source>
+        <translation>Ricarica</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/FirstPage.qml" line="54"/>
+        <source>J2: Enjoyer!</source>
+        <translation>J2: Enjoyer!</translation>
+    </message>
+    <message numerus="yes">
+        <location filename="../qml/pages/FirstPage.qml" line="71"/>
+        <source>sold out of %n</source>
+        <comment>after a number of units, e.g. 1500 [in big] &lt;new line&gt; sold out of 2000</comment>
+        <translation>
+            <numerusform>venduto di %n</numerusform>
+            <numerusform>venduti di %n</numerusform>
+        </translation>
+    </message>
+    <message numerus="yes">
+        <location filename="../qml/pages/FirstPage.qml" line="79"/>
+        <source>%Ln% funded</source>
+        <translation>
+            <numerusform>%Ln% finanziato</numerusform>
+            <numerusform>%Ln% finanziati</numerusform>
+        </translation>
+    </message>
+    <message numerus="yes">
+        <location filename="../qml/pages/FirstPage.qml" line="91"/>
+        <source>The product will not be produced as %n units were not supported by %1</source>
+        <comment>%1 is date</comment>
+        <translation>
+            <numerusform>L&apos;oggetto non sarà prodotto in quanto %n unità non sono state preordinate da %1</numerusform>
+            <numerusform></numerusform>
+        </translation>
+    </message>
+    <message numerus="yes">
+        <location filename="../qml/pages/FirstPage.qml" line="92"/>
+        <source>The product will be produced as %n units were supported by %1</source>
+        <comment>%1 is date</comment>
+        <translation>
+            <numerusform>L&apos;oggetto sarà prodotto in quanto %n unità sono state preordinate da %1</numerusform>
+            <numerusform></numerusform>
+        </translation>
+    </message>
+    <message numerus="yes">
+        <location filename="../qml/pages/FirstPage.qml" line="93"/>
+        <source>The product will only be produced if %n units are supported by %1</source>
+        <comment>%1 is date</comment>
+        <translation>
+            <numerusform>L&apos;oggetto sarà prodotto solo se %n unità verrà venduta al %1</numerusform>
+            <numerusform>L&apos;oggetto sarà prodotto solo se %n unità verranno vendute al %1</numerusform>
+        </translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/FirstPage.qml" line="119"/>
+        <source>ended</source>
+        <comment>after the duration before the deadline, e.g. 3 MONTHS AGO &lt;new line&gt; ended</comment>
+        <translation>finito</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/FirstPage.qml" line="120"/>
+        <source>will end</source>
+        <comment>after the duration before the deadline, e.g. IN 3 MONTHS &lt;new line&gt; will end</comment>
+        <translation>finisce</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsPage</name>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="27"/>
+        <source>Settings</source>
+        <translation>Impostazioni</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="32"/>
+        <source>Enjoyables server</source>
+        <translation>Server Enjoyer</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="54"/>
+        <source>Path to the Enjoyable</source>
+        <translation>Percordo dell&apos;Enjoyer</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="59"/>
+        <source>Jolla Phone Pre-order</source>
+        <translation>Jolla Phone Preordine</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="64"/>
+        <source>Jolla Phone (Sep 2026)</source>
+        <translation>Jolla Phone (Set 2026)</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="74"/>
+        <source>Auto-update</source>
+        <translation>Auto ricarica</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="87"/>
+        <source>Auto-update interval, in seconds: %1</source>
+        <translation>Intervallo auto ricarica di secondi: %1</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="88"/>
+        <source>Auto-update interval, in seconds</source>
+        <translation>Intervallo auto ricarica, in secondi</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="109"/>
+        <source>Auto-update in background</source>
+        <translation>Auto ricarica in background</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="122"/>
+        <source>Background auto-update interval, in seconds: %1</source>
+        <translation>Intervallo auto ricarica in background di secondi: %1</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="123"/>
+        <source>Background auto-update interval, in seconds</source>
+        <translation>Intervallo auto ricarica in background di secondi</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="145"/>
+        <source>Enjoy failures in Events</source>
+        <translation>Fallimenti Enjoy negli Eventi</translation>
+    </message>
+</context>
+<context>
+    <name>harbour-j2enjoyer</name>
+    <message>
+        <location filename="../qml/harbour-j2enjoyer.qml" line="30"/>
+        <source>Enjoying failure!</source>
+        <translation>Fallimento Enjoyer!</translation>
+    </message>
+    <message>
+        <location filename="../qml/harbour-j2enjoyer.qml" line="73"/>
+        <source>Error %1</source>
+        <translation>Errore %1</translation>
+    </message>
+</context>
+</TS>
